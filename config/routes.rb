0ce98e95c57b53c2c9ae3c27257e9d6ebc3e6efa
ShinichiNakajima <ShinichiTheNakaG@gmail.com>
2022-05-17 19:42:38 +0900

    plusrojikaru

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'rojikarus/index'
   devise_for :users
   root to: 'bookmarks#index'
   resources :bookmarks, only: [:index, :new, :create, :show, :destroy] do
